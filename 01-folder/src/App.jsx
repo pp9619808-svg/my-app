5ee1943e339f95506cb2c85a04d2a948ccb34fc6
pp9619808-svg <pp9619808@gmail.com>
2026-04-01 21:04:34 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='dad'>
+      <h1 id='child1'>Hello, World!</h1>
+      <h2 id='child2'>Welcome to React!</h2>
+    </div>
+    <div id="chacha">
+    </div>
+    </>
+  )
+}
+
+
+export default App
